f6c07f041f7d6a5f470408ca5974b8a5ec647fe4
lajka <viktor.bergstram@gmail.com>
2024-04-04 08:06:40 +0200

    push

---
+++ README.md
@@ -0,0 +1,6 @@
+# Lajka's dotfiles
+
+***If your reading this the install script has yet to be tested properly so don't try to run it or you might die!***
+
+
+
--- install.sh
+++ install.sh
@@ -2,65 +2,104 @@
 # Small script to install dotfiles either
 # directly as files or as symlinks.
 
-save () {
-    printf "A backup of your current config will be saved in ./oldconfig\n\n"
+config_folder="${HOME}/.config"
+
+main () {
+    printf "Please select preferred installation method\n"
+    printf "Install dotfiles either [directly] as files or as [symlinks]: "
+
+    local input
+    read input
+    if [[ $input == "symlinks" ]]; then
+        confirm $input
+        install create_file
+
+    elif [[ $input == "directly" ]]; then
+        confirm $input
+        install create_symlink
+
+    else 
+        printf "Invalid option entered, exiting...\n"
+    fi
 }
 
-create_symlink () {
-    printf "${1}, ${2}, symlink\n"
-}
-
-create_file () {
-    printf "${1}, ${2}, file\n"
+confirm () {
+    printf "You have selected installing ${1}, confirm by entering [yes]: "
+    local confirm
+    read confirm
+    if [[ $confirm == "yes" ]]; then
+        return 0
+    else 
+        printf "yes was not entered, exiting...\n"
+        exit 0
+    fi
 }
 
 install () {
-    printf "\nBeginning install\n"
-    save
+    printf "\nBeginning install\n\n"
+    backup_config
 
-    shopt -s dotglob
-    nr_of_files=0
+    local nr_of_files=0
+    local nr_of_installed=0
+
     for file in *; do 
 
-        if [[ -d $file ]] && [[ $file != ".git" ]] && [[ $file != "oldconfig" ]]; then
-            ((nr_of_files++))
-            printf "Installing ${file} in ${config_folder}\n"
-            #$1 $file $config_folder
-            printf "Success\n\n"
-
-        elif [[ $file =~ "rc" ]]; then
-            ((nr_of_files++))
-            printf "Installing ${file} in ${HOME}\n"
-            #$1 $file $HOME
-            printf "Success\n\n"
+        if [[ -d $file ]] && [[ $file != "oldconfig" ]]; then
+            let nr_of_files++
+            if $1 $file $config_folder; then
+                let nr_of_installed++
+            fi    
+            printf "\n"
         fi
     done
-    shopt -u dotglob
-    printf "Finished installing $nr_of_files out of $nr_of_files\n"
+    printf "Finished installing $nr_of_installed out of $nr_of_files\n"
 }
 
-config_folder="${HOME}/.config"
+backup_config () {
+    printf "A backup of your current ~/.config will be saved in ./oldconfig\n"
 
-printf "Please select preferred installation method\n"
-printf "Install dotfiles either [directly] as files or as [symlinks]: "
-read input
-
-if [[ $input == "symlinks" ]]; then
-    printf "You have selected installing symlinks, confirm by entering [yes]: "
-    read confirm
-    if [[ $confirm == "yes" ]]; then
-        install create_symlink
+    if cp -r $config_folder ./oldconfig; then
+        printf "Backup successfully created\n\n" 
+    else 
+        printf "Backup failed, exiting...\n" 
+        exit 1
     fi
+}
 
-elif [[ $input == "directly" ]]; then
-    printf "You have selected installing directly, confirm by entering [yes]: "
-    if [[ $confirm == "yes" ]]; then
-        install create_file
+create_symlink () {
+    printf "Creating symlink for ${1} in ${2}\n"
+    if [[ -f "${2}/${1}" ]]; then
+        printf "Found already existing ${1} config\n"
+        printf "Overwrite it? [yes]/[no]: "
+        local overwrite
+        read overwrite
+        if [[ $overwrite == "yes" ]]; then
+            ln -sf $1 $2
+            printf "Symlink successfully created\n"
+        fi
+    else 
+        ln -s $1 $2
+        printf "Symlink successfully created\n"
     fi
+    return 0
+}
 
-else 
-    printf "Invalid option!\n"
-fi
+create_file () {
+    printf "Creating config for ${1} in ${2}\n"
+    if [[ -f "${2}/${1}" ]]; then
+        printf "Found already existing ${1} config\n"
+        printf "Overwrite it? [yes]/[no]: "
+        local overwrite
+        read overwrite
+        if [[ $overwrite == "yes" ]]; then
+            cp -rf $1 $2
+            printf "File successfully created\n"
+        fi
+    else 
+        cp -r $1 $2
+        printf "File successfully created\n"
+    fi
+    return 0
+}
 
-
-
+main "$@"
